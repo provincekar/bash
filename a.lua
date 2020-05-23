b0dce8bb609c8b390c4474eba3ed13fc5d05382d
provincekar <65734863+provincekar@users.noreply.github.com>
2020-05-24 00:53:46 +0300

    Add files via upload

--- a.lua
+++ a.lua
@@ -1,27 +1,81 @@
-gg.setRanges(gg.REGION_C_ALLOC)
-gg.setVisible(false)
-gg.searchNumber("67109377", gg.TYPE_DWORD)
-gg.isVisible(false)
-gg.getResults(50000)
-gg.setVisible(false)
-gg.editAll("0", gg.TYPE_DWORD)
-gg.setVisible(false)
 gg.clearResults()
-gg.setRanges(gg.REGION_C_ALLOC)
-gg.setVisible(false)
-gg.searchNumber("67109633", gg.TYPE_DWORD)
-gg.isVisible(false)
-gg.getResults(50000)
-gg.setVisible(false)
-gg.editAll("0", gg.TYPE_DWORD)
-gg.setVisible(false)
-gg.clearResults()
-gg.setRanges(gg.REGION_C_ALLOC)
-gg.setVisible(false)
-gg.searchNumber("2506", gg.TYPE_DWORD)
-gg.isVisible(false)
-gg.getResults(50000)
-gg.setVisible(false)
-gg.editAll("0", gg.TYPE_DWORD)
-gg.setVisible(false)
-gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC)
+  gg.searchNumber("67109377;12547::100", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("12547", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  jg = gg.getResults(100)
+  sl = gg.getResultCount()
+  if 100 < sl then
+    sl = 100
+  end
+  for i = 1, sl do
+    dzy = jg[i].address
+          gg.addListItems({
+        [1] = {
+          address = dzy,
+          flags = gg.TYPE_DWORD,
+          freeze = true,
+          value = 0
+        }
+      })
+  end
+  gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC)
+  gg.searchNumber("0;0~20;131586::9", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("131586", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  jg = gg.getResults(100)
+  sl = gg.getResultCount()
+  if 100 < sl then
+    sl = 100
+  end
+  for i = 1, sl do
+    dzy = jg[i].address
+              gg.addListItems({
+        [1] = {
+          address = dzy,
+          flags = gg.TYPE_DWORD,
+          freeze = true,
+          value = 0
+        }
+      })
+  end
+  gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC)
+  gg.searchNumber("67109377;12547::100", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("12547", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  jg = gg.getResults(100)
+  sl = gg.getResultCount()
+  if 100 < sl then
+    sl = 100
+  end
+  for i = 1, sl do
+    dzy = jg[i].address
+              gg.addListItems({
+        [1] = {
+          address = dzy,
+          flags = gg.TYPE_DWORD,
+          freeze = true,
+          value = 0
+        }
+      })
+  end
+  gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC)
+  gg.searchNumber("0;0~20;131586::9", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("131586", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  jg = gg.getResults(100)
+  sl = gg.getResultCount()
+  if 100 < sl then
+    sl = 100
+  end
+  for i = 1, sl do
+    dzy = jg[i].address
+              gg.addListItems({
+        [1] = {
+          address = dzy,
+          flags = gg.TYPE_DWORD,
+          freeze = true,
+          value = 0
+        }
+      })
+  end
+  gg.clearResults()
