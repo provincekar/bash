1c9897346fad2ad571f9f4b125302a520314b380
provincekar <65734863+provincekar@users.noreply.github.com>
2020-06-04 02:03:16 +0300

    Add files via upload

--- a.lua
+++ a.lua
@@ -8,11 +8,13 @@ end
 function HOME()
   MN = gg.choice({
     "ANTIBAN 🔥",
+    "HIZLI ON/OFF",
     "ÇIKIŞ ❌",
   }, nil, "GaminggodsTR VIP ANTIBAN")
   if MN == nil then else
     if MN == 1 then antiban() end
-    if MN == 2 then exit() end
+    if MN == 2 then onoff() end
+    if MN == 3 then exit() end
   end
   
   PUBGMH = -1
@@ -39,6 +41,21 @@ end
 gg.addListItems((gg.getResults(30101996, nil, nil, nil, nil, nil, nil, nil, nil)))
 gg.clearResults()
 end
+
+function onoff()
+gg.clearResults()
+gg.setRanges(gg.REGION_C_DATA)
+gg.searchNumber("1.1754945e-37",gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0,-1)
+gg.getResults(1000)
+gg.editAll("0",gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0,-1)
+gg.getResults(1000)
+gg.editAll("0",gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0,-1)
+gg.searchNumber("0",gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0,-1)
+gg.getResults(1000)
+gg.editAll("1.1754945e-37",gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0,-1)
+gg.clearResults()
+end
+
 
 function exit()
 print('Iyi oyunlar')
